12fe5f47e69b6c1529cf10b679d3ecfa7e046cf8
asusrob <robins_r88@hotmail.com>
2023-03-24 11:25:06 -0500

    Prueba entornos

--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
-
 /**
  * npm init
  * npm install --save express
  * npm i -D nodemon
+ * npm i dotenv
  */
+require('dotenv').config()
 
 const express = require('express') //llamamos a Express
 const app = express()            
 
-//const port = process.env.PORT || 8080
 const port = 3003
 
 app.use( express.urlencoded({extended:false}) )
@@ -17,7 +17,15 @@ app.use(express.json())
 const router = require('./routes/routes')
 app.use('/test', router)
 
+const envi = process.env.NODE_ENV
+const conf = {
+	dev: { port: process.env.PORT_DEV },
+	qa: { port: process.env.PORT_QA },
+	prod: { port: process.env.PORT_PROD },
+}
 
 app.listen(port, function () {
 	console.log(`Servidor corriendo en el puerto: ${port}`);
+	console.log(`---> ENV: ${envi}`);
+	console.log(`---> ENV puerto: ${conf[envi].port}`);
 })
